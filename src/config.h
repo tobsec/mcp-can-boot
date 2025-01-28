5184cdb9c37baf039430a7ac33c00b8f996bb0a3
tobsec <8700196+tobsec@users.noreply.github.com>
2025-01-28 16:25:29 +0100

    Adapt BL

--- src/config.h
+++ src/config.h
@@ -12,15 +12,17 @@
 #ifndef	__MCP_CAN_BOOT_CONFIG_H__
 #define	__MCP_CAN_BOOT_CONFIG_H__
 
+#define MCU_ID_EEPROM_ADDR 256
+
 /**
  * The ID of the MCU to identify it in bootloader CAN messages.
  * You may use a fixed value or an expressions to get the ID from the EEPROM
  * which allows you to change the ID by your main application.
  * Range: 0x0000 to 0xFFFF
  */
-#define MCU_ID 0x0042
-//#define MCU_ID eeprom_read_word((uint16_t*) 0x00)
-//#define MCU_ID eeprom_read_byte((uint8_t*) 0x00)
+// #define MCU_ID 0x0042
+#define MCU_ID eeprom_read_word((uint16_t*) MCU_ID_EEPROM_ADDR)
+//#define MCU_ID eeprom_read_byte((uint8_t*) MCU_ID_EEPROM_ADDR)
 
 /**
  * Timeout for the bootloader in milliseconds.
@@ -28,7 +30,7 @@
  * received via CAN to enter the bootloading mode. Otherwise the main
  * application will be started.
  */
-#define TIMEOUT 250
+#define TIMEOUT 1000
 
 /**
  * Bitrate of the CAN bus.
@@ -36,7 +38,7 @@
  * CAN_50KBPS, CAN_80KBPS, CAN_83K3BPS, CAN_95KBPS, CAN_100KBPS, CAN_125KBPS,
  * CAN_200KBPS, CAN_250KBPS, CAN_500KBPS or CAN_1000KBPS
  */
-#define CAN_KBPS CAN_500KBPS
+#define CAN_KBPS CAN_125KBPS
 
 /**
  * Set multiple bitrates to let the bootloader try to detect the bitrate used on
@@ -68,7 +70,7 @@
  * Clock speed of the MCP2515 CAN controller.
  * MCP_8MHZ, MCP_16MHZ or MCP_20MHZ
  */
-#define MCP_CLOCK MCP_16MHZ
+#define MCP_CLOCK MCP_8MHZ
 
 /**
  * Optional definition of a custom CS (chip select) pin for the MCP2515.
@@ -91,19 +93,19 @@
  * Use the Extended Frame Format (EFF) for CAN-Messages. (8 hex chars)
  * Set to `false` to use the Standard Frame Format (SFF). (3 hex chars)
  */
-#define CAN_EFF true
+#define CAN_EFF false
 
 /**
  * CAN-ID for bootloader message from MCU to remote.
  */
-#define CAN_ID_MCU_TO_REMOTE 0x1FFFFF01UL
-//#define CAN_ID_MCU_TO_REMOTE 0x1F1
+// #define CAN_ID_MCU_TO_REMOTE 0x1FFFFF01UL
+#define CAN_ID_MCU_TO_REMOTE 0x1F1
 
 /**
  * CAN-ID for bootloader message from remote to MCU.
  */
-#define CAN_ID_REMOTE_TO_MCU 0x1FFFFF02UL
-//#define CAN_ID_REMOTE_TO_MCU 0x1F2
+// #define CAN_ID_REMOTE_TO_MCU 0x1FFFFF02UL
+#define CAN_ID_REMOTE_TO_MCU 0x1F2
 
 /**
  * Optional definition of a LED port, which will be used to indicate
@@ -112,6 +114,9 @@
 //#define LED      PORTA1
 //#define LED_DDR  DDRA
 //#define LED_PORT PORTA
+// #define LED      PORTB5
+// #define LED_DDR  DDRB
+// #define LED_PORT PORTB
 
 /**
  * Store the MCU Status Register to Register 2 (R2).
